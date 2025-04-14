b331ce3c2f61295274df3e8baca23909f433e683
nika123w <nikusha.kurtiashvili@gmail.com>
2025-04-14 08:53:18 -0700

    Create Post.java

---
+++ Post.java
@@ -0,0 +1,10 @@
+package com.anonbook.model;
+
+import java.sql.Timestamp;
+
+public class Post {
+    public int id;
+    public String content;
+    public String photoPath;
+    public Timestamp createdAt;
+}
